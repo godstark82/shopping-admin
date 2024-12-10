9eea9291b9df66fde300cbba4f4120ffc5b263ef
VENOM LEADER <79461207+godstark82@users.noreply.github.com>
2024-12-11 01:07:00 +0530

    updated env

--- src/services/firebase/config.js
+++ src/services/firebase/config.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
+// Initialize Firebase if not already initialized
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 
